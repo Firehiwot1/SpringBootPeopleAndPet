6ce4dc06a7245ceecc0b5a92b967dab0756e1ce1
Firehiwot1 <fmazengia@gmail.com>
2019-07-22 17:11:43 -0400

    first initial

---
+++ src/main/java/com/example/demo/PeoplePetsRepository.java
@@ -0,0 +1,8 @@
+package com.example.demo;
+
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface PeoplePetsRepository extends CrudRepository<PeoplePets,Long> {
+    Iterable<PeoplePets> findAllByOwner_Id(Long owner_id);
+}
---
+++ src/main/java/com/example/demo/SpringBootPeopleAndPetsApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPeopleAndPetsApplication {
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(SpringBootPeopleAndPetsApplication.class, args);
+	}
+
+}
